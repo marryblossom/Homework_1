a0f900103b090ce47bba1a5a8b3ac517e6077ecf
mali.tian <mali.tian@rea-group.com>
2016-01-15 02:07:22 +0800

    do some refactor

--- src/main/data_helper.rb
+++ src/main/data_helper.rb
@@ -6,27 +6,24 @@ module DataHelper
     def initialize (input)
       @input = input
       @days = @input.split(': ')[1].split(',')
-      @weekday_count = 0
-      @weekend_count = 0
+      @customer_type = input.split(': ')[0]
     end
 
-    def all_hotels (customer_type)
-      lakewood_hotel = Hotel.new('Lakewood', 3, customer_type == 'Regular' ? 110 : 80, customer_type == 'Regular' ? 90 : 80)
-      bridgewood_hotel = Hotel.new('Bridgewood', 4, customer_type == 'Regular' ? 160 : 110, customer_type == 'Regular' ? 60 : 50)
-      ridgewood_hotel = Hotel.new('Ridgewood', 5, customer_type == 'Regular' ? 220 : 100, customer_type == 'Regular' ? 150 : 40)
+    def all_hotels
+      lakewood_hotel = Hotel.new('Lakewood', 3, @customer_type == 'Regular' ? 110 : 80, @customer_type == 'Regular' ? 90 : 80)
+      bridgewood_hotel = Hotel.new('Bridgewood', 4, @customer_type == 'Regular' ? 160 : 110, @customer_type == 'Regular' ? 60 : 50)
+      ridgewood_hotel = Hotel.new('Ridgewood', 5, @customer_type == 'Regular' ? 220 : 100, @customer_type == 'Regular' ? 150 : 40)
       hotels = [lakewood_hotel, bridgewood_hotel, ridgewood_hotel]
       hotels
     end
 
-    def customer_type
-      @input.split(': ')[0]
-    end
-
     def days_count
+      weekday_count = 0
+      weekend_count = 0
       @days.each do |day|
-        is_weekend(day)? @weekend_count += 1 : @weekday_count += 1
+        is_weekend(day)? weekend_count += 1 : weekday_count += 1
       end
-      {weekday_count: @weekday_count, weekend_count: @weekend_count}
+      {weekday_count: weekday_count, weekend_count: weekend_count}
     end
 
     private
--- src/main/reservation.rb
+++ src/main/reservation.rb
@@ -5,19 +5,22 @@ class Reservation
   def initialize (input)
     @reservation_data_helper = DataHelper::ReservationDataHelper.new(input)
     @hotels_with_price_and_rating = []
-    @customer_type = @reservation_data_helper.customer_type
     @days_count = @reservation_data_helper.days_count
   end
 
   def cheapest_hotel
-     hotels = @reservation_data_helper.all_hotels(@customer_type)
+     hotels = @reservation_data_helper.all_hotels
      hotels.each do |hotel|
-       @hotels_with_price_and_rating << {'name' => hotel.name, 'price' => hotel_price(hotel), 'rating' => hotel.rating}
+       @hotels_with_price_and_rating << hotel_with_price_and_rating(hotel)
      end
      sort_by_price_and_rating[0]['name']
   end
 
   private
+
+  def hotel_with_price_and_rating (hotel)
+    {'name' => hotel.name, 'price' => hotel_price(hotel), 'rating' => hotel.rating}
+  end
 
   def hotel_price (hotel)
     @days_count[:weekday_count].to_i * hotel.weekday_rate + @days_count[:weekend_count].to_i * hotel.weekend_rate
